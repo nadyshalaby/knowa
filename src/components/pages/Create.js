808f2d8bbdbeaf1759eedbbe3be567b20452ef27
nady shalaby <nady80878@gmail.com>
2018-11-18 14:08:38 +0200

    sdkfjckxjckx

--- src/components/pages/Create.js
+++ src/components/pages/Create.js
@@ -25,9 +25,27 @@ export default class Create extends Component {
             </div>
 
             <div className="col-md-12 mb-3">
-              <div class="form-group">
+              <div className="form-group">
                 <label>Article Content</label>
-                <textarea class="form-control" rows="5" />
+                <textarea className="form-control" rows="5" />
+              </div>
+            </div>
+
+            <div className="col-md-4 mb-3">
+              <div className="input-group">
+                <select
+                  className="custom-select"
+                  aria-label="Example select with button addon">
+                  <option selected disabled>
+                    Choose Category...
+                  </option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+                <div className="input-group-append">
+                  <button className="btn btn-outline-secondary" type="button" />
+                </div>
               </div>
             </div>
           </div>
