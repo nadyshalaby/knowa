88ad2af104e2dcc723db30e5c10d74540978f377
nady shalaby <nady80878@gmail.com>
2018-11-18 13:32:43 +0200

    sdjkjdsd

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "./articles/Navbar";
+import Create from "./articles/Create";
 
 const App = () => (
   <div>
     <Navbar />
+    <Create></Create> 
   </div>
 );
 
